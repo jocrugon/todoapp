06a39692a420f970f458eaf9b4cf75eb5ab79856
José Darío González Cruz <darioucv@ucvvirtual.edu.pe>
2024-02-17 11:40:42 -0500

    marcar y eliminar elemento en lista

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { Task } from './../models/task.model';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  tasks = signal<Task[]>([
+    {
+      id:Date.now(),
+      title:'Crear Proyecto',
+      completed: false
+    },
+    {
+      id:Date.now(),
+      title:'Crear Componentes',
+      completed: false
+    }
+  ]);
 
+  changeHandler(event:Event){
+    const input = event.target as HTMLInputElement;
+    const newTask = input.value;
+    this.addTask(newTask);
+  }
+
+  addTask(title:string){
+    const newTask ={
+      id:Date.now(),
+      title:title,
+      completed:false
+    }
+    this.tasks.update((tasks)=>[...tasks,newTask]);//patrón no mutar, crear nuevos estados
+  }
+
+  deleteTask(index:number){
+    this.tasks.update((tasks)=>tasks.filter((task,position)=>position!==index));
+  }
+
+  updateTask(index:number){
+    this.tasks.update((tasks) => {
+      return tasks.map((task,position) => {
+        if(position ===index){
+          return{
+            ...task,
+            completed: !task.completed
+
+          }
+        }
+        return task;
+      })
+    })
+  }
 }
--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -9,10 +9,32 @@ import { CommonModule } from '@angular/common';
   styleUrl: './labs.component.css'
 })
 export class LabsComponent {
-  name = 'José Darío González Cruz';
-  tasks = [
+  name = signal('José Darío González Cruz');
+  tasks = signal([
     'install angula CLI',
     'create project',
     'create components'
-  ];
+  ]);
+
+  age = 24;
+  disabled = true;
+  img = 'https://w3schools.com/howto/img_avatar.png';
+  person = {
+    name:'dario',
+    age: 24,
+    avatar: 'https://w3schools.com/howto/img_avatar.png'
+  }
+  clickHandler(){
+    alert('Hola');
+  }
+
+  changeHandler(event:Event){
+    const input = event.target as HTMLInputElement;
+    const newValue = input.value;
+    this.name.set(newValue)
+  }
+  keydownHandler(event:KeyboardEvent){
+    const input = event.target as HTMLInputElement;
+    console.log(input.value);
+  }
 }
