1d1c636f4320d8e54a4615719eca6e88744a5211
José Darío González Cruz <darioucv@ucvvirtual.edu.pe>
2024-02-22 02:23:35 -0500

    todo app completed

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, Injector, computed, effect, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { Task } from './../models/task.model';
@@ -12,18 +12,7 @@ import { FormControl, ReactiveFormsModule, Validators} from '@angular/forms';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  tasks = signal<Task[]>([
-    {
-      id:Date.now(),
-      title:'Crear Proyecto',
-      completed: false
-    },
-    {
-      id:Date.now(),
-      title:'Crear Componentes',
-      completed: false
-    }
-  ]);
+  tasks = signal<Task[]>([]);
 
   filter = signal<'all'|'pending'|'completed'>('all');
   tasksByFilter = computed(()=>{
@@ -44,6 +33,29 @@ export class HomeComponent {
       Validators.required,
     ]
   })
+
+  injector = inject(Injector);
+
+  constructor(){
+    
+  }
+
+  ngOnInit(){
+    const storage = localStorage.getItem('tasks');
+    if(storage){
+      const tasks = JSON.parse(storage);
+      this.tasks.set(tasks);
+    }
+    this.trackTasks();
+  }
+
+  trackTasks(){
+    effect(()=>{
+      const tasks = this.tasks();
+      console.log(tasks);
+      localStorage.setItem('tasks',JSON.stringify(tasks));
+    },{injector:this.injector});
+  }
 
   changeHandler(){
     if(this.newTaskCtrl.valid){
